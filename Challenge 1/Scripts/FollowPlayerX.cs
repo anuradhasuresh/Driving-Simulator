0240279caec5f60b248898054ee23fcdc89380a9
anuradhasuresh <55852292+anuradhasuresh@users.noreply.github.com>
2023-04-26 17:35:31 -0400

    Add files via upload

---
+++ Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(18.08f, 0.33f, 3.16f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
